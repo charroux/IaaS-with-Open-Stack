19f78b4d6dcedf451bdc48f910f4bab6923cb7f2
charroux <benoit.charroux@free.fr>
2015-10-31 10:26:44 +0100

    first commit

---
+++ databaseInstallation.sh
@@ -0,0 +1,5 @@
+yes | yum install java-1.7.0-openjdk-devel
+yes | yum install wget
+yes | yum install unzip
+wget http://sourceforge.net/projects/hsqldb/files/hsqldb/hsqldb_1_8_1/hsqldb_1_8_1_3.zip
+unzip hsqldb_1_8_1_3.zip
